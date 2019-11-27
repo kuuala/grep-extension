9707e9a26dbd783da71d3da274a3eb2230f1a75a
kuuuala <kumaritof@gmail.com>
2019-11-27 13:45:37 +0300

    Main -> Starter

--- src/main/java/ru/kuuuala/grep/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.kuuuala.grep;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("GOGOGO");
-    }
-}
---
+++ src/main/java/test/grepgui/Starter.java
@@ -0,0 +1,24 @@
+package test.grepgui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("My Grep GUI");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+}
