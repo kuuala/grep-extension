95124c60839255d0ac8e8a863f50889103a9c22b
kuuala <kumaritof@ya.ru>
2019-11-28 18:17:56 +0300

    change ResultWindow and controller

--- src/main/java/test/grepgui/controllers/ResultWindowController.java
+++ src/main/java/test/grepgui/controllers/ResultWindowController.java
@@ -1,10 +1,28 @@
 package test.grepgui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TreeView;
 
 public class ResultWindowController {
 
     @FXML
+    private TreeView resultTreeView;
+    @FXML
+    private TabPane resultTabPane;
+
+    @FXML
+    private void upClickButton() {
+
+    }
+
+    @FXML
+    private void downClickButton() {
+
+    }
+
+    @FXML
     private void copyAllClickButton() {
+
     }
 }
